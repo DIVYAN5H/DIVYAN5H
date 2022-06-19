d61829d53d427a6ada5fc40329f668eea0b011e3
Divyansh <divyanshvermafast4@gmail.com>
2022-06-20 00:47:24 +0530

    under progress projects

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -16,20 +16,36 @@ function myFunc(data) {
     data.forEach(data => {
         let card = document.createElement('div')
         card.className = 'project-card'
-        card.innerHTML = `
-    <div class="card-title">
-    ${data.title}
-    </div>
-    <div class="card-description">
-    ${data.desc}
-    </div>
-      <div class="card-button button">
-        <a href="${data.url}" target="_blank">
+        if (data.url !== '#') {
+            card.innerHTML = `
+            <div class="card-title">
+            ${data.title}
+            </div>
+            <div class="card-description">
+            ${data.desc}
+            </div>
+            <div class="card-button button">
+            <a href="${data.url}" target="_blank">
             Live <img src="./assets/images/right.svg" alt="" class="button-logo card-button-logo">
-        </a>
-      </div>
-      `
-      cardContainer.append(card)
+            </a>
+            </div>
+            `
+        }
+        else {
+            card.innerHTML = `
+            <div class="card-title">
+            ${data.title}
+            </div>
+            <div class="card-description">
+            ${data.desc}
+            </div>
+            <div class="card-button button">
+            <a href="${data.url}">
+            Under Progress
+            </a>
+            </div>
+            `
+        }
+        cardContainer.append(card)
     });
-
 }
